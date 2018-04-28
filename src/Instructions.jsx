214d35e05d129c719158ae9273ff7dec00a164a3
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-28 23:44:21 +0300

    Add files via upload

---
+++ src/Instructions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Instructions = () => (
+
+  <div id="headerwrap">
+    <p>Welcome, Oleg.</p>
+    <p>Please use top menu to accomplish your tasks.</p>
+  </div>
+
+);
+
+export default Instructions;
