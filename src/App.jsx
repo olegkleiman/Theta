c5a883b3ecaaa2d10651b293575b07fd04e0a3c3
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-28 14:03:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,60 @@
+// Flow
 import React from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-class App extends React.Component {
+type Props = {
+
+}
+
+type State = {
+  userName: string
+}
+
+class App extends React.Component<Props,State> {
+
+  state = {
+    userName: '',
+  };
+
+  responseGoogle = (response) => {
+
+    console.log(response);
+
+    if( response.profileObj ) {
+      this.setState({
+        userName: response.profileObj.name
+      });
+    }
+
+  }
+
+  logout = () => {
+    this.setState({
+      userName: ''
+    });
+  }
 
   render() {
 
-    return (<div>Theta</div>);
+    if( this.state.userName ) {
+      return <React.Fragment>
+                <GoogleLogout
+                    buttonText="Logout"
+                    onLogoutSuccess={::this.logout}>
+                  </GoogleLogout>
+                  <div className='loginText'>{this.state.userName}</div>
+              </React.Fragment>
+    } else {
+
+      return (<GoogleLogin
+                clientId="110875185211-0d0fu4nji6ph5jkvqf14eafb8u6lvime.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={::this.responseGoogle}
+                onFailure={::this.responseGoogle}>
+                  <i className='fab fa-google'></i>
+                  <span className='loginText'> Login with Google</span>
+             </GoogleLogin>)
+    }
 
   }
 
