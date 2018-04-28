c6b5f42a6f431a172a5568008e855d213315e04b
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-28 23:31:24 +0300

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+
+  return (<div className='container'>
+                <div className='row justify-content-md-center'>
+                    <div className='col col-md-1 text-center'>
+                      <i className='fab fa-facebook'></i>
+                    </div>
+                    <div className='col col-md-3'>
+                      <div>Made by Ktulhu Inc., 2018</div>
+                    </div>
+                </div>
+            </div>);
+
+};
+
+export default Footer;
