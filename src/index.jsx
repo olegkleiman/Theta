39ba8890da75d79871d2d24a0e6c55009128cac9
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-28 15:32:59 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { HashRouter as Router}  from 'react-router-dom';
 
+import reducers from './reducers';
 import App from './App';
 
-ReactDOM.render(<App />,
+let store = createStore(reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+ReactDOM.render(<Provider store={store}>
+                  <Router>
+                    <App />
+                  </Router>
+                </Provider>,
                 document.getElementById('root'));
 
 module.hot.accept();
