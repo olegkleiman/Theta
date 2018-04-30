f6b364fc023b7cdd72b663c87d5d6df7df9b40f2
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-05-01 00:46:20 +0300

    Update UserList.jsx

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import firebase from './firebase.js';
 import { Card } from 'reactstrap';
 import { EditingState } from '@devexpress/dx-react-grid';
 import {
@@ -32,9 +34,77 @@ class UserList extends React.PureComponent {
          columnValues: { id: ({ index }) => index, ...defaultColumnValues },
          length: 8,
        }),
+       users: {}
    };
 
     this.commitChanges = this.commitChanges.bind(this);
+  }
+
+  componentDidMount() {
+
+    // var provider = new firebase.auth.GoogleAuthProvider();
+    // firebase.auth().languageCode = 'he';
+    // firebase.auth().signInWithPopup(provider).then(function(result) {
+    //       // This gives you a Google Access Token. You can use it to access the Google API.
+    //       var token = result.credential.accessToken;
+    //       // The signed-in user info.
+    //       var user = result.user;
+    //       // ...
+    //     }).catch(function(error) {
+    //       // Handle Errors here.
+    //       var errorCode = error.code;
+    //       var errorMessage = error.message;
+    //       // The email of the user's account used.
+    //       var email = error.email;
+    //       // The firebase.auth.AuthCredential type that was used.
+    //       var credential = error.credential;
+    //       // ...
+    //     });
+
+    firebase.auth().signInAndRetrieveDataWithCustomToken(this.props.accessToken)
+                    .catch( error => {
+                      var errorCode = error.code;
+                      var errorMessage = error.message;
+                    });
+
+    // firebase.auth().signInAnonymously()
+    //                 .then( result => {
+    //                   console.log(result)
+    //                 })
+    //                 .catch( error => {
+    //                   var errorMessage = error.message;
+    //                 });
+    firebase.auth().onAuthStateChanged( (user) => {
+      if( user ) {
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+      }
+    });
+
+    this.firebaseRef = firebase.database().ref('/users');
+    this.firebaseCallback = this.firebaseRef.on('value', (snap) => {
+      this.setState({
+        users: snap.val()
+      })
+    });
+
+    this.unregisterCollectionObserver = firebase.firestore().collection('users').onSnapshot( (snap) => {
+
+      const collection = {};
+
+      snap.forEach( (docSnapshot) => {
+        collection[docSnapshot.id] = docSnapshot.data();
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    // Unregister the listener on '/users'.
+    this.firebaseRef.off('value', this.firebaseCallback);
+
+    if( this.unregisterCollectionObserver ) {
+      this.unregisterCollectionObserver();
+    }
   }
 
   commitChanges({ added, changed, deleted }) {
@@ -89,4 +159,10 @@ class UserList extends React.PureComponent {
 
 };
 
-export default UserList;
+function mapStateToProps(state) {
+  return {
+    accessToken: state.jwt,
+  }
+}
+
+export default connect(mapStateToProps)(UserList);
