154ecb266dbace85c8efa54f8d07d04c5e3f1b58
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-05-04 23:51:17 +0300

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -14,90 +14,26 @@ import Footer from './Footer';
 import dashboardRoutes from './routes/dashboard.jsx';
 
 import Sidebar from './Sidebar';
+import Header from './Header';
 
 class Dashboard extends React.Component {
 
-  // componentWillReceiveProps(nextProps){
-  //
-  // }
-
-  logout = () => {
-
-    firebaseApp.auth().signOut();
-
-    this.props.dispatch({
-      type: 'LOGOUT'
-    });
-
-    this.props.history.push('');
-
-  }
-
   render() {
       return (
-        <React.Fragment>
+        <div className="wrapper">
           <Sidebar {...this.props} routes={dashboardRoutes}/>
-          <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-              <Link className='navbar-brand' to='/dashboard'>Theta</Link>
-              <button className="navbar-toggler" type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item dropdown">
-                    <Link className='nav-link dropdown-toggle' data-toggle='dropdown' to='/dashboard/lists'>
-                      Lists <span className="sr-only caret">(current)</span>
-                    </Link>
-                    <ul className='dropdown-menu' role='menu'>
-                      <li>
-                        <a href='#'>One</a>
-                      </li>
-                      <li>
-                        <a href='#'>Two</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link className='nav-link' to='/dashboard/import'>ייבוא נתונים</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className='nav-link' to='/dashboard/reports'>דו״חות</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to='/dashboard/users'>משתמשים</Link>
-                  </li>
-                </ul>
-                <div className="form-inline my-2 my-lg-0">
-                    <img src={this.props.userPictureUrl}
-                      className="img-fluid rounded-circle avatar" alt="User avatar" />
-                    <button className='btn'
-                       onClick={::this.logout}>
-                      <i className="fa fa-google-plus pr-1"></i> Logout
-                    </button>
-
-                </div>
-              </div>
-          </nav>
-          <div className='container'>
-
+          <div className="main-panel" ref="mainPanel">
+            <Header {...this.props}/>
             <Switch>
-              <Route exact path='/dashboard' render={ (props) => <Instructions userName={this.props.userName} /> } />
-              <Route path={this.props.match.path + '/lists'} component={Lists} />
-              <Route path={this.props.match.path + '/users'} component={UserList} />
-              <Route path={this.props.match.path + '/import'} component={ImportData} />
-              <Route path={this.props.match.path + '/reports'} component={Reports} />
+                <Route exact path='/dashboard' render={ (props) => <Instructions userName={this.props.userName} /> } />
+                <Route path={this.props.match.path + '/lists'} component={Lists} />
+                <Route path={this.props.match.path + '/users'} component={UserList} />
+                <Route path={this.props.match.path + '/import'} component={ImportData} />
+                <Route path={this.props.match.path + '/reports'} component={Reports} />
             </Switch>
-
+            <Footer fluid/>
           </div>
-
-          <Footer fluid/>
-        </React.Fragment>);
+        </div>);
   }
 
 };
