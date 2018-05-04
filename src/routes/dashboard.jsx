1c719aba0b26e05c34f7bd8e7d684d85878f9240
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-05-04 23:54:18 +0300

    Create dashboard.jsx

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,13 @@
+import Dashboard from '../views/Dashboard/Dashboard.jsx';
+import UserList from '../UserList';
+import ImportWizard from '../ImportWizard';
+import ReportsWizard from '../ReportsWizard';
+
+var dashRoutes = [
+    { path: "/dashboard", name: "Home", icon: "design_app", component: Dashboard },
+    { path: "/dashboard/users", name: "Users", icon: "users_single-02", component: UserList },
+    { path: "/dashboard/import", name: "Import", icon: "arrows-1_cloud-upload-94", component: ImportWizard },
+    { path: "/dashboard/reports", name: "Reports", icon: "design_bullet-list-67", component: ReportsWizard },
+    { redirect: true, path: "/", pathTo: "/dashboard", name: "Dashboard" }
+];
+export default dashRoutes;
