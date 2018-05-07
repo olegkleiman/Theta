030561738d175f27bd21060b4db8b3db49782a8d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-05-08 01:34:53 +0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,18 +1,25 @@
+// flow
 import React from 'react';
+import { Container } from 'reactstrap';
 
-const Footer = () => {
+const Footer = (props) => {
 
-  return (<div className='container'>
-                <div className='row justify-content-md-center'>
-                    <div className='col col-md-1 text-center'>
-                      <i className='fab fa-facebook'></i>
-                    </div>
-                    <div className='col col-md-3'>
-                      <div>Made by Ktulhu Inc., 2018</div>
-                    </div>
+  return (
+    <footer className={"footer"
+        + (props.default ? " footer-default":"")
+    }>
+      <Container fluid={props.fluid ? true:false}>
+          <div className='row justify-content-md-center'>
+              <div className='col'>
+                <div className="copyright">
+                    &copy; {1900 + (new Date()).getYear()}, Designed by <a href="https://www.invisionapp.com" target="_blank" rel="noopener noreferrer">Invision</a>
                 </div>
-            </div>);
 
+              </div>
+          </div>
+        </Container>);
+    </footer>
+  )
 };
 
 export default Footer;
