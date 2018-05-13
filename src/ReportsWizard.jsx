e8203294bfd337bf12edb44d7847c722f699f5f7
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-05-14 00:09:57 +0300

    Update ReportsWizard.jsx

--- src/ReportsWizard.jsx
+++ src/ReportsWizard.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class ReportsWizard extends React.Component {
 
   render() {
-    return (<div>DDD</div>)
+    return (<div>
+              <div className='panel-header panel-header-sm'></div>
+            </div>)
   }
 
 };
