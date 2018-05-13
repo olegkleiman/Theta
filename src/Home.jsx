256e04bcc5be409e9104a6a1b3496fd6889eaf1f
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-05-13 03:06:33 +0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -57,23 +57,39 @@ class Home extends React.Component<State, Props> {
   }
 
   render() {
-    return (<Container className='containerShift'>
-                <Row>
-                  {
-                    this.state.activities.map( (activity, index) => {
-                        return (
-                          <div className='col-3' key={index}>
-                            <div className='card card-user'>
-                              <div className='card-body'>
-                                <h5 className='card-title'>{activity.name}</h5>
-                                <p className='card-text'>{activity.description}</p>
-                              </div>
-                            </div>
-                          </div>)
-                    })
-                  }
-                </Row>
-            </Container>)
+    return (<div>
+              <div className='panel-header panel-header-sm'></div>
+              <div className='content'>
+                  <Row>
+                    <div className='col col-md-12'>
+                      <div className='card'>
+                        <div className='card-header'>
+                          <h5 className='title'>Your daily tasks</h5>
+                          <p className='category'>Sorted by recent activity</p>
+                        </div>
+                        <div className='card-body'>
+                          <Row>
+                            {
+                              this.state.activities.map( (activity, index) => {
+                                  return (
+                                    <div className='col-3' key={index}>
+                                      <div className='card card-user'>
+                                        <div className='card-body'>
+                                          <h5 className='card-title'>{activity.name}</h5>
+                                          <p className='card-text'>{activity.description}</p>
+                                        </div>
+                                      </div>
+                                    </div>)
+                              })
+                            }
+                          </Row>
+                        </div>
+                      </div>
+                    </div>
+                  </Row>
+
+              </div>
+            </div>)
   }
 
 };
