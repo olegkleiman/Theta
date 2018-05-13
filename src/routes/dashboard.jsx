e3ee53a9368742e375f96c2d3515bf9900250695
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-05-13 23:35:01 +0300

    Update dashboard.jsx

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -4,9 +4,9 @@ import ImportWizard from '../ImportWizard';
 import ReportsWizard from '../ReportsWizard';
 
 var dashRoutes = [
-    { path: "/dashboard", name: "Home", icon: "design_app", component: Dashboard },
-    { path: "/dashboard/users", name: "Users", icon: "users_single-02", component: UserList },
-    { path: "/dashboard/import", name: "Import", icon: "arrows-1_cloud-upload-94", component: ImportWizard },
-    { path: "/dashboard/reports", name: "Reports", icon: "design_bullet-list-67", component: ReportsWizard }
+    { path: "/dashboard", name: "Home", id: 1, icon: "design_app", component: Dashboard },
+    { path: "/dashboard/users", name: "Users", id: 2, icon: "users_single-02", component: UserList },
+    { path: "/dashboard/import", name: "Import", id: 3, icon: "arrows-1_cloud-upload-94", component: ImportWizard },
+    { path: "/dashboard/reports", name: "Reports", id: 4, icon: "design_bullet-list-67", component: ReportsWizard }
 ];
 export default dashRoutes;
