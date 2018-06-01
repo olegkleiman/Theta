0b554aad86dbb745abfc4641f77f42853a06ff27
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-06-01 15:55:56 +0300

    Update Office.jsx

--- src/Office.jsx
+++ src/Office.jsx
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from "react-router-dom";
 
 import {
   Row
@@ -54,4 +53,4 @@ class Office extends React.Component {
 
 }
 
-export default withRouter(connect()(Office));
+export default connect()(Office);
