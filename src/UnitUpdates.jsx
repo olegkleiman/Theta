69da963a95230ccfd42af956e4e16dc61338f379
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-06-06 04:14:59 +0300

    Add files via upload

---
+++ src/UnitUpdates.jsx
@@ -0,0 +1,123 @@
+// @flow
+import React from 'react';
+import moment from 'moment';
+import firebase from './firebase.js';
+import {
+  EditingState,
+  IntegratedFiltering,
+  PagingState,
+  IntegratedPaging,
+  SortingState,
+  SearchState } from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  Toolbar,
+  SearchPanel,
+  TableHeaderRow,
+  TableEditRow,
+  TableEditColumn,
+  PagingPanel
+} from '@devexpress/dx-react-grid-bootstrap4';
+
+type State = {
+  columns: [],
+  updates: [],
+  sorting: []
+}
+
+type Props = {
+  docId: String
+}
+
+const getRowId = row => row.id;
+
+class UnitUpdates extends React.Component<Props, State> {
+
+  state = {
+    columns: [
+      { name: 'update_date', title: 'update date' },
+      { name: 'pupils', title: 'students' },
+      { name: 'places', title: 'places' }
+    ],
+    //rows: _rows,
+    updates: [],
+    sorting: [{ columnName: 'pupils', direction: 'asc' }]
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    if( prevProps.docId !== this.props.docId) {
+
+      const getOptions = {
+        source: 'server'
+      }
+
+      let _updates = [];
+
+      firebase.firestore().collection('units').doc(this.props.docId)
+      .get(getOptions)
+      .then( doc => {
+
+        let data = doc.data();
+        data.updates.forEach( update => {
+
+          _updates.push({
+            update_date: moment(update.update_date.seconds*1000).format('MM/DD/YYYY'),
+            places: update.places,
+            pupils: update.pupils
+          })
+        });
+
+        this.setState({
+          updates: _updates
+        });
+
+      });
+
+    }
+
+  }
+
+  commitChanges({ added, changed, deleted }) {
+  }
+
+  render() {
+
+    const { updates, columns, sorting } = this.state;
+
+    return (
+      <Grid
+        rows={updates}
+        columns={columns}
+        getRowId={getRowId}>
+        <PagingState
+          defaultCurrentPage={0}
+          pageSize={3}
+        />
+        <IntegratedFiltering />
+        <IntegratedPaging />
+        <EditingState
+          onCommitChanges={::this.commitChanges}
+        />
+
+        <SortingState
+          sorting={sorting} />
+
+        <Table />
+        <TableHeaderRow showSortingControls />
+        <TableEditRow />
+        <TableEditColumn
+          showAddCommand
+          showEditCommand
+          showDeleteCommand
+        />
+
+        <Toolbar />
+      </Grid>
+    )
+  }
+
+}
+
+export default UnitUpdates;
