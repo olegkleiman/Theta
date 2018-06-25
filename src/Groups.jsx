13a15440216b75f6cb496814922eb5f59170f587
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-06-25 04:08:35 +0300

    Update Groups.jsx

--- src/Groups.jsx
+++ src/Groups.jsx
@@ -1,4 +1,8 @@
+// @flow
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import ReactTable from 'react-table';
+import firebase from './firebase.js';
 
 import {
   Container,
@@ -17,9 +21,64 @@ import {
   Label
 } from 'reactstrap';
 
-class Groups extends React.Component {
+type State = {
+  groups: [{
+    symbol: String
+  }],
+}
+
+type Props = {
+
+}
+
+class Groups extends React.Component<Props, State> {
+
+  state = {
+    groups: []
+  }
+
+  componentDidMount() {
+
+    const unitId = this.props.match.params.unitid;
+
+    const getOptions = {
+      source: 'server'
+    }
+
+    const self = this;
+
+    firebase.firestore().collection('units/' + unitId + '/groups')
+      .get(getOptions)
+      .then( response => {
+
+        const _groups = [];
+
+        response.docs.forEach( (group) => {
+          const groupData = group.data();
+          _groups.push({
+            symbol: groupData.symbol
+          });
+        });
+
+        self.setState({
+          groups: _groups
+        })
+      });
+  }
+
+  onRowSelected = (rowInfo) => {
+    console.log(rowInfo);
+  }
 
   render() {
+
+    const columns = [{
+      Header: 'Symbol',
+      accessor: 'symbol'
+    }];
+
+    const self = this;
+
     return <div>
               <div className='panel-header panel-header-sm'></div>
               <div className='content container h-100'>
@@ -28,6 +87,20 @@ class Groups extends React.Component {
                     <div className='card'>
                       <div className='card-header'>
                         <h5 className='title'>Groups</h5>
+                      </div>
+                      <div>
+                        <ReactTable
+                          getTrProps={(state, rowInfo, column) => {
+                              return {
+                                onClick: (e, handleOriginal) => {
+                                  self.onRowSelected(rowInfo);
+                                  if( handleOriginal )
+                                    handleOriginal();
+                                }
+                              }
+                          }}
+                          data={this.state.groups}
+                          columns={columns}/>
                       </div>
                     </div>
                   </div>
@@ -38,4 +111,4 @@ class Groups extends React.Component {
 
 }
 
-export default Groups;
+export default withRouter(Groups)
