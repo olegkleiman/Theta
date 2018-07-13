472f9b2fc0d54adb709515cdf98e7be292e09574
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-13 16:35:14 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,17 @@ var config = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          }
+        ]
       }
     ]
   },
