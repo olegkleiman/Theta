81175bdf2d16da3b3617bec4c4763437de85a9ec
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-22 03:40:16 +0300

    Update AddGroup.jsx

--- src/AddGroup.jsx
+++ src/AddGroup.jsx
@@ -1,19 +1,13 @@
 // @flow
 import React from 'react';
-import {
-  Button,
-  Row,
-  Col,
-  Form,
-  FormGroup,
-  Input,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+import { Button, Row, Col, Container, Form, FormGroup,
+  Card, CardBody, CardTitle,
+  Input, InputGroup, InputGroupAddon
 } from 'reactstrap';
 import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
 import moment from 'moment';
+import _ from 'moment/locale/he';
 import firebase from './firebase.js';
 
 type State = {
@@ -52,7 +46,8 @@ class AddGroup extends React.Component {
       name: event.target.groupName.value,
       symbol: event.target.symbol.value,
       capacity: event.target.groupCapacity.value,
-      opened: this.state.fromDate.toDate()
+      opened: this.state.fromDate.toDate(),
+      price: event.target.price.value
     }
 
     const unitId = this.props.match.params.unitid;
@@ -60,7 +55,28 @@ class AddGroup extends React.Component {
     firebase.firestore().collection('units').doc(unitId).collection('groups')
     .add(group)
     .then( doc => {
-      console.log(doc);
+      return doc;
+    })
+    .then( doc => {
+
+      // 1 - Open
+      // 2 - Till date was expired
+      // 3 - Groups is full
+      // 4 - Close
+
+      return fetch('http://rishumon.com/api/elamayn/edit_class.php', {
+        method: 'POST',
+        body:
+            {
+            	"groupSymbol": group.symbol,
+            	"description": group.name,
+            	"status": "1",
+            	"price": group.price
+        }
+      })
+    })
+    .then( resp => {
+      console.log(resp);
     })
     .catch( err => {
       console.error(err);
@@ -74,71 +90,89 @@ class AddGroup extends React.Component {
       <div className='content container h-100'>
         <Row>
           <Col className='col-md-12'>
-            <div className='card'>
+            <Card body className="text-center">
               <div className='card-header'>
                 <h5 className='title'>Create new Group</h5>
               </div>
-              <div className='card-body'>
-                <Row>
-                  <div className='card'>
-                    <div className='card-body'>
+              <CardBody>
+                <Card>
+                    <CardBody>
                       <Form onSubmit={::this.onFormSubmit}>
-                        <Row>
-                          <Col className='col-md-2'>
-                            <div className='info-text'>Group Name</div>
-                          </Col>
-                          <Col className='col-md-4'>
-                            <Input id='groupName' name='groupName'></Input>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col>
-                            Symbol
-                          </Col>
-                          <Col>
-                            <Input id='symbol' name='symbol'></Input>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col>
-                            Opened From
-                          </Col>
-                          <Col>
-                            <Datetime closeOnSelect={true}
-                                      onChange={::this._fromDateChanged}
-                                      local='he' />
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col>
-                            Closed At
-                          </Col>
-                          <Col>
-                            <Datetime closeOnSelect={true}
-                                      onChange={::this._tillDateChanged}
-                                      local='he' />
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col>
-                            Capacity
-                          </Col>
-                          <Col>
-                            <Input id='groupCapacity' name='groupCapacity'></Input>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <div className='col col-lg-1'>
-                            <Button type="submit" color='success'>Create</Button>
-                          </div>
-                        </Row>
-
+                        <Container>
+                          <Row>
+                            <Col md={{ size: 2, offset: 2 }} className="text-right">
+                              <div className='info-text'>Group Name</div>
+                            </Col>
+                            <Col md={{ size: 4 }}>
+                              <Input id='groupName' name='groupName'></Input>
+                            </Col>
+                          </Row>
+                          <br />
+                          <Row>
+                            <Col md={{ size: 2, offset: 2 }} className="text-right">
+                              Symbol
+                            </Col>
+                            <Col md='4'>
+                                <Input id='symbol' name='symbol'
+                                        type='number' placeholder="only numbers" />
+                            </Col>
+                          </Row>
+                          <br />
+                          <Row>
+                            <Col md={{ size: 2, offset: 2 }} className="text-right">
+                              Opened From
+                            </Col>
+                            <Col md='4'>
+                              <Datetime closeOnSelect={true}
+                                        onChange={::this._fromDateChanged}
+                                        local='he' />
+                            </Col>
+                          </Row>
+                          <br />
+                          <Row>
+                            <Col md={{ size: 2, offset: 2 }} className="text-right">
+                              Closed At
+                            </Col>
+                            <Col md='4'>
+                              <Datetime closeOnSelect={true}
+                                        onChange={::this._tillDateChanged}
+                                        local='he' />
+                            </Col>
+                          </Row>
+                          <br />
+                          <Row>
+                            <Col md={{ size: 2, offset: 2 }} className="text-right">
+                              Capacity
+                            </Col>
+                            <Col md='4'>
+                              <Input id='groupCapacity' name='groupCapacity'
+                                     type='number' placeholder="only numbers" />
+                            </Col>
+                          </Row>
+                          <br />
+                          <Row>
+                            <Col md={{ size: 2, offset: 2 }} className="text-right">
+                              Price
+                            </Col>
+                            <Col md='4'>
+                              <InputGroup>
+                                <Input id='price' name='price'
+                                       type='number' placeholder="only numbers as money, e.g. 670, 670.65" />
+                                <InputGroupAddon addonType="append">₪</InputGroupAddon>
+                              </InputGroup>
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col md={{ size: 1, offset: 10}}>
+                              <Button type="submit" color='primary'>Create</Button>
+                            </Col>
+                          </Row>
+                        </Container>
                       </Form>
-                    </div>
-                  </div>
-                </Row>
-              </div>
-            </div>
+                    </CardBody>
+                </Card>
+              </CardBody>
+            </Card>
           </Col>
         </Row>
       </div>
