1d385236af9e1d567d509a74b43f347a6e3ed112
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-23 14:42:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from './Login';
+import Logout from './Logout';
 import Dashboard from './Dashboard';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     return(
       <Switch>
         <Route exact path='/' component={Login} />
+        <Route path='/logout' component={Logout} />
         <Route path='/dashboard' component={Dashboard} />
       </Switch>)
 
