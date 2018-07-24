9e6262c268f57927129b257c730a76c59fbdf5cd
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-24 19:58:20 +0300

    Update Units.jsx

--- src/Units.jsx
+++ src/Units.jsx
@@ -1,107 +1,49 @@
 // @flow
 import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import firebase from './firebase.js';
-import { Button, Row, Col, Input,
-  Dropdown, DropdownToggle, DropdownMenu, DropdownItem
-} from 'reactstrap';
+import classNames from 'classnames';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import Unit from './Unit';
+import Groups from './Groups';
+import withAuth from './FirebaseAuth';
 
-import UnitUpdates from './UnitUpdates';
-import UnitGroups from './UnitGroups';
+import { Button, Row, Col } from 'reactstrap';
 
 type State = {
-  docData: {
+  units: [],
+  selectedUnit: {
     name: String,
-    authority: String,
-    concessionaire: String,
-    symbol: String,
-    type: String,
-    education_type: String,
-    long_day_permit: Boolean,
-    status: String
+    id: String
   },
-  docId: String,
+  selectedRowIndex: Number,
+  dropdownOpen: boolean,
+  dataStatus: string
 }
 
-type Props = {
-  docId: String
-}
-
-class Unit extends React.Component<Props, State> {
+class Units extends React.Component<{}, State> {
 
   state = {
-    docData: {
+    units: [],
+    selectedUnit: {
       name: '',
-      authority: '',
-      concessionaire: '',
-      symbol: '',
-      type: '',
-      education_type: '',
-      long_day_permit: false,
-      status: ''
+      id: ''
     },
-    docId: '',
+    selectedRowIndex: -1,
+    dropdownOpen: false,
+    dataStatus: 'טוען נתונים...'
+  };
+
+  toggle() {
+     this.setState(prevState => ({
+       dropdownOpen: !prevState.dropdownOpen
+     }));
   }
 
-  gotoGroups() {
-    this.props.dispatch({
-      type: 'PAGE_NAVIGATED',
-      data: {
-        pageName: 'Groups of ' + this.state.docData.name
-      }
-    });
+  async componentDidUpdate(prevProps: Props, prevState: State) {
 
-    this.props.history.push('/dashboard/groups/' + this.props.docId);
-
-  }
-
-  componentDidMount() {
-
-     this._loadAsyncData(this.props.docId);
-
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-
-    if( prevProps.docId !== this.props.docId ) {
-      this._loadAsyncData(this.props.docId)
-    }
-
-  }
-
-  // static getDerivedStateFromProps(props, state) {
-  //
-  //   if( props.docId !== state.docId ) {
-  //
-  //     const getOptions = {
-  //       source: 'server'
-  //     }
-  //
-  //     return firebase.firestore().collection('units').doc(props.docId)
-  //       .get(getOptions)
-  //       .then( doc => {
-  //
-  //         let data = doc.data();
-  //
-  //         return{
-  //             docData: data,
-  //             docId: props.docId
-  //         }
-  //
-  //       });
-  //
-  //   } else {
-  //
-  //     // Return null to indicate no changes to state
-  //     return null;
-  //   }
-  //
-  // }
-
-  _loadAsyncData(docId: String) {
-
-    if( docId !== this.props.id ) {
+    if( this.props.secRoles.length != prevProps.secRoles.length ) {
+      const userRoles = this.props.secRoles;
 
       const getOptions = {
         source: 'server'
@@ -109,122 +51,141 @@ class Unit extends React.Component<Props, State> {
 
       const self = this;
 
-      firebase.firestore().collection('units').doc(docId)
-        .get(getOptions)
-        .then( doc => {
+      const response = await firebase.firestore().collection('units')
+                         .get(getOptions);
 
-          let data = doc.data();
+      const _units = [];
 
-          self.setState({
-              docData: data,
-              docId: docId
-          })
+      response.docs.forEach( (unit) => {
 
+        const unitData = unit.data();
+
+        const secRole = unitData.sec_role;
+        const isSecGroupFound = userRoles.find( role => {
+          return role === secRole
         });
+
+        if( isSecGroupFound ) {
+          _units.push({
+            name: unitData.name_he,
+            concessionaire: unitData.concessionaire,
+            symbol: unitData.symbol,
+            id: unit.id
+          });
+        }
+
+      });
+
+      this.setState({
+        units: _units,
+        dataStatus: _units.length == 0 ? 'No Units are allowed to view for this account'
+                                        : this.state.dataStatus
+      })
     }
   }
 
-  addGroup() {
-      this.props.history.push('/dashboard/addgroup/' + this.state.docId);
+  onUnitSelected = (unit) => {
+    this.setState({
+      selectedUnit: unit
+    })
+  }
+
+  onRowSelected = (rowInfo) => {
+
+    this.setState({
+      selectedUnit: {
+        name: rowInfo.original.name,
+        id: rowInfo.original.id
+      },
+      selectedRowIndex: rowInfo.index
+    });
   }
 
   render() {
 
-    // No changes are permitted for uncontrolled inputs within this form.
-    // Accordingly, we use'value' prop for these inputs set to the appropriate
-    // values in state. Such design does not allow actual reflecting user typing.
-    // It seems better that making all input disabled.
-    // If you wabt to enable changes for some input, provide 'defaultValue' props
-    // provide ref for such input and set its the 'value' after getting
-    // docDate from firebase
+    let nextButtonClassName = classNames('btn btn-next btn-fill btn-success btn-wd', {
+      'disabled': this.state.selectedUnit.name == ''
+    });
+
+    const dropdownTitle = this.state.selectedUnit.name == '' ? 'Select Unit'
+                                                          : this.state.selectedUnit.name;
+
+    let unit = this.state.selectedUnit.id == '' ? null
+                : <Unit docId={this.state.selectedUnit.id} />
+
+    const self = this;
+
+    const columns = [{
+      Header: 'מזהה',
+      accessor: 'id'
+    },{
+      Header: 'שם',
+      accessor: 'name'
+    }, {
+      Header: 'סמל',
+      accessor: 'symbol',
+    }, {
+      Header: 'זכיין',
+      accessor: 'concessionaire'
+    }];
 
     return <div>
-              <div className='card'>
-                <ul className='nav nav-tabs lustify-content-center' role='tablist'>
-                  <li className='nav-item'>
-                    <a className='nav-link active' data-toggle='tab' href='#general'
-                        role='tab' area-expanded='true'>
-                      <i className='now-ui-icons ui-2_settings-90'></i>הגדרות כלליות
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link' data-toggle='tab' href='#updates'
-                      role='tab' area-expanded='false'>
-                      <i className='now-ui-icons ui-1_calendar-60'></i>עדכונים
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link' data-toggle='tab' href='#groups'
-                      role='tab' area-expanded='false'>
-                      <i className='now-ui-icons education_hat'></i>כיתות
-                    </a>
-                  </li>
-                </ul>
-                <div className='card-body'>
-                  <div className='tab-content text-center'>
-                    <div id='general' className='tab-pane active' role='tabpanel'>
-                      <Row>
-                        <Col className='text-left'>
-                          <label className='form-control-label'>שם</label>
-                          <Input type='text' value={this.state.docData.name_he} />
-                        </Col>
-                        <Col className='text-left'>
-                          <label className='form-control-label'>רשות</label>
-                          <Input type='text' value={this.state.docData.authority} />
-                        </Col>
-                        <Col className='text-left'>
-                          <label className='form-control-label'>זכיין</label>
-                          <Input type='text' value={this.state.docData.concessionaire} />
-                        </Col>
-                        <Col className='text-left'>
-                          <label className='form-control-label'>סמל</label>
-                          <Input type='text' value={this.state.docData.symbol} />
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col xs='2' className='text-left'>
-                          <label className='form-control-label'>סוג</label>
-                          <Input type='text' value={this.state.docData.type} />
-                        </Col>
-                        <Col xs='3' className='text-left'>>
-                          <label className='form-control-label'>סוג חינוך</label>
-                          <Input type='text' value={this.state.docData.education_type} />
-                        </Col>
-                        <Col xs='3' className='text-left'>
-                          <label className='form-control-label'>אישור יוח"א</label>
-                          <Input type='radio' checked readOnly value={this.state.docData.long_day_permit}
-                                className='form-control'/>
-                        </Col>
-                        <Col xs='4' className='text-left'>
-                          <label className='form-control-label'>סטאטוס</label>
-                          <Input type='text' value={this.state.docData.status} />
-                        </Col>
-                      </Row>
-                    </div>
-                    <div id='updates' className='tab-pane' role='tabpanel'>
-                      <UnitUpdates docId={this.state.docId} />
-                    </div>
-                    <div id='groups' className='tab-pane' role='tabpanel'>
-
-                        <div className="card card-body">
-                          <div className='row'>
-                              <div className='col col-12 d-flex justify-content-end'>
-                              <Button color='primary'
-                                      className='align-self-end'
-                                      onClick={::this.addGroup}>הוסף כיתה +</Button>
-                              </div>
+              <div className='panel-header panel-header-sm'></div>
+              <div className='content container h-100'>
+                <Row>
+                  <div className='col col-md-12'>
+                    <div className='card'>
+                      <div className='card-header'>
+                        <h5 className='title'>ניהול מוסדות</h5>
+                      </div>
+                      <div className='card-body'>
+                        <Row>
+                          <div className='card'>
+                            <div className='card-body'>
+                            <ReactTable
+                              getTrProps={(state, rowInfo, column) => {
+                                  return {
+                                    onClick: (e, handleOriginal) => {
+                                      self.onRowSelected(rowInfo);
+                                      if( handleOriginal )
+                                        handleOriginal();
+                                    },
+                                    style: {
+                                      background: rowInfo && rowInfo.index === this.state.selectedRowIndex ?
+                                                  'rgba(0,0,0,0.05)' : 'white',
+                                      cursor: 'pointer',
+                                    }
+                                  }
+                              }}
+                              getTheadThProps = { () => {
+                                return {
+                                  style: {
+                                    'textAlign': 'right'
+                                  }
+                                }
+                              }}
+                              data={this.state.units}
+                              noDataText={this.state.dataStatus}
+                              columns={columns}
+                              minRows={5}
+                              showPagination={false}
+                              className="-highlight col col-12"/>
                           </div>
-                          <UnitGroups docId={this.state.docId} />
-
-                        </div>
-
+                          </div>
+                        </Row>
+                        <Row>
+                          <Col xs='12'>
+                            {unit}
+                          </Col>
+                        </Row>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Row>
               </div>
            </div>
   }
 
 }
 
-export default withRouter(connect()(Unit));
+export default withAuth(Units);
