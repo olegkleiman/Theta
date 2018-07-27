47d7e91a74a0d630aabae7bea073163bbf07a6e2
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-28 02:39:25 +0300

    Update units.js

--- input/units.js
+++ input/units.js
@@ -3,85 +3,85 @@ const readline = require('readline');
 var parse = require('csv-parse');
 require('firebase/firestore');
 import * as admin from 'firebase-admin';
-var serviceAccount = require("./theta-1524876066401-firebase-adminsdk-8cprg-1edd7d9a67.json");
+var serviceAccount = require("./theta-restore-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://theta-1524876066401.firebaseio.com"
+  databaseURL: "https://thetadev-49eb5.firebaseio.com"
 });
 
 const firestore = admin.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
 var inputFile = 'units.csv'
 
-console.log('Started');
+const secRoles = [];
 
-var parser = parse({delimiter: ','}, function (err, data) {
+var parser = parse({delimiter: ','}, async (err, data) => {
 
   if( err ) {
     console.log(err);
     return;
   }
 
+  const promises = data.map( async(row,index) => {
 
+    let symbol = row[3];
+    if( index !== 0 && symbol != '' ) {
 
-
-
-
-  data.forEach( (row, index) => {
-
-    const symbol = row[3];
-    if( symbol != '' ) {
+      if( symbol.indexOf('-') !== -1 ) {
+        symbol = symbol.replace('-', '');
+      }
 
       console.log(`Index: ${index}. Data: ${row}`);
       const sec_role = 'unit_' + row[3];
-      console.log(sec_role);
+      console.log(`Symbol ${symbol}. Sec Role: ${sec_role}`);
 
-      // firestore.collection('units').add({
-      //   name_he: row[5],
-      //   authority: row[1],
-      //   symbol: symbol,
-      //   sec_role: 'unit_' + symbol,
-      //   education_type: row[6],
-      //   type: row[7]
-      // })
-      //.then( res => {
-
-        firestore.collection('users')
-                  .get()
-                  .then( snap => {
-
-                    snap.forEach( doc => {
-                      var userRef = firestore.collection("users").doc(doc.id);
-                      const userData = doc.data();
-
-                      const secRoles = userData.sec_roles;
-
-                      const found  = secRoles.find( role => {
-                          return role === sec_role
-                      });
-
-                      if( !found ) {
-
-                        secRoles.push(sec_role);
-                        console.log(`New roles ${userData.sec_roles}`);
-
-                        userRef.update({
-                          sec_roles: secRoles
-                        })
-                      }
-                    })
-
-                  })
-
-      //})
-      .catch( err => {
-        console.error(err);
+      const res = await firestore.collection('units').add({
+        name_he: row[5],
+        authority: row[1],
+        symbol: symbol,
+        sec_role: sec_role,
+        education_type: row[6],
+        type: row[7]
       })
 
+      return sec_role;
     }
 
   });
 
+  let secRoles = await Promise.all(promises);
+  secRoles = secRoles.filter( r => r); // remove nulls
+  rolesUpdater(secRoles);
+
 });
 
-fs.createReadStream(inputFile).pipe(parser);
+async function rolesUpdater(secRoles) {
+
+  const promises = secRoles.map( async(row, index) => {
+
+      const usersSnap = await firestore.collection('users').get();
+      usersSnap.forEach( doc => {
+        var userRef = firestore.collection("users").doc(doc.id);
+        const userData = doc.data();
+        console.log(userData.first_name);
+
+        userRef.update({
+             sec_roles: [...userData.sec_roles, ...secRoles]
+        })
+        .then( res => {
+          console.log(res);
+        })
+
+      });
+
+  });
+
+  await Promise.all(promises);
+  console.log('Promises resolved');
+
+};
+
+fs.createReadStream(inputFile)
+.pipe(parser);
