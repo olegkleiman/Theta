15bef4a01769f6e744b4e982dd42ac57169aca98
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-29 04:26:17 +0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,21 +1,20 @@
 // flow
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 const Footer = (props) => {
 
   return (
     <footer className="page-footer">
       <Container fluid={props.fluid ? true:false}>
-        <nav>
-          <ul>
-            <li>אודותינו</li>
-          </ul>
-        </nav>
-        <div className="copyright">
-            &copy; {1900 + (new Date()).getYear()}, Design based on <a href="https://www.creative-tim.com/live/now-ui-dashboard-pro-react" target="_blank" rel="noopener noreferrer">Now UI Kit</a>
-        </div>
-        </Container>
+        <Row>
+          <Col md='12' className='text-right'>
+            <div className="copyright">
+                &copy; {1900 + (new Date()).getYear()}, Design based on <a href="https://www.creative-tim.com/live/now-ui-dashboard-pro-react" target="_blank" rel="noopener noreferrer">Now UI Kit</a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 };
