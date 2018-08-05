067010b9f9f0fd80468d38eac6480fa3eb8ae9cf
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-06 02:30:45 +0300

    Update Groups.jsx

--- src/Groups.jsx
+++ src/Groups.jsx
@@ -2,117 +2,301 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import ReactTable from 'react-table';
+import Datetime from 'react-datetime';
 import moment from 'moment';
+import XLSX from 'xlsx';
 import firebase from './firebase.js';
-
-import {
-  Container,
-  Button,
-  Row,
-  Card,
-  Input,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  Form,
-  FormText,
-  FormControl,
-  FormGroup,
-  Label
+import Pagination from './TablePagination';
+import { Container, Button,
+  Row, Col, Card, CardHeader, CardBody,
+  Tooltip
 } from 'reactstrap';
 
-type State = {
-  groups: [{
+type Group = {
+    id: String,
+    unitId: String,
+    name: String,
     symbol: String,
-    opened: Date
-  }],
+    capacity: Number,
+    open: Date,
+    openTill: Date,
+    unitName: String,
+    price: Number
 }
 
-type Props = {
-
+type State = {
+  groups: Group[],
+  tooltipOpen: Boolean
 }
 
-class Groups extends React.Component<Props, State> {
+class Groups extends React.Component<{}, State> {
 
   state = {
-    groups: []
+    groups: [],
+    tooltipOpen: false
   }
 
-  componentDidMount() {
-
-    const unitId = this.props.match.params.unitid;
+  async componentDidMount() {
 
     const getOptions = {
       source: 'server'
     }
 
-    const self = this;
+    const _groups = [];
+    const units = await firebase.firestore().collection('units')
+                       .get(getOptions);
 
-    firebase.firestore().collection('units/' + unitId + '/groups')
-      .get(getOptions)
-      .then( response => {
+    units.docs.forEach( async(unit) => {
 
-        const _groups = [];
+      const unitData = unit.data();
+      const unitId = unit.id;
+      const unitName = unitData.name_he;
 
-        response.docs.forEach( (group) => {
+      const groups = await firebase.firestore().collection('units')
+                    .doc(unit.id).collection('groups')
+                    .get(getOptions);
+      groups.docs.forEach( group => {
+
           const groupData = group.data();
-          _groups.push({
-            symbol: groupData.symbol,
-            opened: moment.unix(groupData.opened.seconds).format('DD/MM/YYYY')
-          });
-        });
+          const groupId = group.id;
 
-        self.setState({
-          groups: _groups
-        })
+          const openTill = groupData.openedTill ?
+                           moment.unix(groupData.openedTill.seconds).format('DD/MM/YYYY') :
+                           '';
+
+          _groups.push({
+            id: groupId,
+            unitId: unitId,
+            name: groupData.name,
+            symbol: groupData.symbol,
+            unitName: unitName,
+            open: moment.unix(groupData.opened.seconds).format('DD/MM/YYYY'),
+            openTill: openTill,
+            price: groupData.price,
+            capacity: groupData.capacity
+          });
+
       });
+
+    });
+
+    this.setState({
+      groups: _groups
+    })
+
   }
 
-  onRowSelected = (rowInfo) => {
-    console.log(rowInfo);
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
+
+  exportExcel() {
+
+    const _export = {
+      /* Array of Arrays e.g. [["a","b"],[1,2]] */
+     data: [
+       ["", "מחיר", "ת.סיום", "ת. התחלה", "שם מוסד", "כמות ילדים", "מזהה", "שם"],
+     ],
+    };
+
+    this.state.groups.forEach( (group, index) => {
+        const groupData = [];
+        groupData.push(1 + index); // reserve 1 for caption row
+        groupData.push(group.name);
+        groupData.push(group.symbol);
+        groupData.push(group.capacity);
+        groupData.push(group.unitName);
+        groupData.push(group.open);
+        groupData.push(group.openTill);
+        groupData.push(group.price);
+
+        _export.data.push(groupData);
+    });
+
+    /* create a new blank workbook */
+    var workbook = XLSX.utils.book_new();
+    console.log(workbook.Views);
+    /* convert from array of arrays to workbook */
+    var worksheet = XLSX.utils.aoa_to_sheet(_export.data);
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'כיתות');
+
+    /* create view to RTL */
+    if(!workbook.Workbook) workbook.Workbook = {};
+    if(!workbook.Workbook.Views) workbook.Workbook.Views = [];
+    if(!workbook.Workbook.Views[0]) workbook.Workbook.Views[0] = {};
+    workbook.Workbook.Views[0].RTL = true;
+
+    /* write a workbook */
+    XLSX.writeFile(workbook, 'groups.xlsx');
+
+  }
+
+  async updateFirestore(groupIndex: Number,
+                        groupId: String,
+                        unitId: String,
+                        fieldName: String,
+                        value) {
+
+     const data = [...this.state.groups];
+     data[groupIndex][fieldName] = value;
+     this.setState({
+       groups: data
+     });
+
+     try {
+        let json = {};
+        const updateField = fieldName;
+        json[updateField] = value;
+
+        await firebase.firestore().collection('units')
+                        .doc(unitId).collection('groups')
+                        .doc(groupId)
+                        .update(json);
+     } catch( err ) {
+       console.error(err);
+     }
+
+  }
+
+  async handleUpdate(cellInfo, e) {
+    if( e.key === 'Enter' || e.type === 'blur') {
+
+      e.preventDefault();
+
+      const value = e.target.innerHTML;
+      this.updateFirestore(cellInfo.index,
+                           cellInfo.original.id,
+                           cellInfo.original.unitId,
+                           cellInfo.column.id,
+                           value);
+    }
+  }
+
+  renderEditable(cellInfo, value) {
+    return (
+      <div
+        style={{ backgroundColor: "#fafafa" }}
+        contentEditable
+        suppressContentEditableWarning
+        onKeyDown={ e => ::this.handleUpdate(cellInfo, e) }
+        onBlur={ e => ::this.handleUpdate(cellInfo, e) }>
+        {value}
+      </div>)
+  }
+
+  openDateChaned(_date: Date,
+                 cellInfo) {
+
+    this.updateFirestore(cellInfo.index,
+                         cellInfo.original.id,
+                         cellInfo.original.unitId,
+                         cellInfo.column.id,
+                         _date.unix());
+
+  }
+
+  renderDatePicker(cellInfo, value) {
+    return(
+        <Datetime closeOnSelect={true}
+                  value={value}
+                  onChange={ (_date) => ::this.openDateChaned(_date, cellInfo) }
+                  input={true}
+                  timeFormat={false}
+                  local='he' />
+    );
   }
 
   render() {
 
     const columns = [{
-      Header: 'Symbol',
+      Header: 'שם',
+      accessor: 'name',
+      Cell: cellInfo => ::this.renderEditable(cellInfo, cellInfo.original.name),
+    }, {
+      Header: 'מזהה',
       accessor: 'symbol'
     }, {
-      Header: 'Opened',
-      accessor: 'opened'
+      Header: 'כמות ילדים',
+      accessor: 'capacity'
+    }, {
+      Header: 'שם מוסד',
+      accessor: 'unitName'
+    }, {
+      Header: 'ת. התחלה',
+      accessor: 'open',
+      Cell: cellInfo => ::this.renderDatePicker(cellInfo, cellInfo.original.open),
+      style: {
+        overflow: 'visible'
+      }
+    }, {
+      Header: 'ת.סיום',
+      accessor: 'openTill'
+    }, {
+      Header: 'מחיר',
+      accessor: 'price',
+      Cell: cellInfo => ::this.renderEditable(cellInfo, cellInfo.original.price)
     }];
 
     const self = this;
 
     return <div>
               <div className='panel-header panel-header-sm'></div>
-              <div className='content container h-100'>
+              <Container className='content h-100'>
                 <Row>
-                  <div className='col col-md-12'>
-                    <div className='card'>
-                      <div className='card-header'>
-                        <h5 className='title'>Groups</h5>
-                      </div>
-                      <div>
-                        <ReactTable
-                          getTrProps={(state, rowInfo, column) => {
-                              return {
-                                onClick: (e, handleOriginal) => {
-                                  self.onRowSelected(rowInfo);
-                                  if( handleOriginal )
-                                    handleOriginal();
-                                }
+                  <Col md='12'>
+                    <Card>
+                      <CardHeader>
+                        <h5 className='title'>ניהול כיתות</h5>
+                      </CardHeader>
+                      <CardBody>
+                        <Row>
+                          <Col md='12' className='text-right my-auto' id='tooltipContainer'>
+                              <Button color='primary' id='btnExportExcel'
+                                      onClick={::this.exportExcel}>Excel</Button>
+                              <Tooltip placement='auto'
+                                autohide={false}
+                                isOpen={this.state.tooltipOpen}
+                                toggle={::this.toggle}
+                                container='tooltipContainer'
+                                style={{
+                                  backgroundColor: 'black',
+                                  color: 'white'
+                                }}
+                                target='btnExportExcel'>
+                                  ייצוא לקובץ חיצוני
+                              </Tooltip>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col md='12'>
+                            <ReactTable
+                          filterable
+                          PaginationComponent={Pagination}
+                          getTheadThProps = { () => {
+                            return {
+                              style: {
+                                'textAlign': 'right'
                               }
+                            }
                           }}
+                          loadingText='טוען נתונים...'
+                          noDataText='טוען נתונים...'
+                          previousText = 'קודם'
+                          nextText = 'הבא'
+                          pageText = 'עמוד'
+                          ofText = 'מתוך'
+                          rowsText = 'שורות'
                           data={this.state.groups}
-                          noDataText="טוען נתונים..."
                           columns={columns}/>
-                      </div>
-                    </div>
-                  </div>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+                  </Col>
                 </Row>
-              </div>
+              </Container>
          </div>
   }
 
