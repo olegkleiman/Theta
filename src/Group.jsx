625702b55421dafd6c966bf63d7d034adcd58583
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-06 00:05:19 +0300

    Update Group.jsx

--- src/Group.jsx
+++ src/Group.jsx
@@ -1,153 +1,94 @@
-// flow
+// @flow
 import React from 'react';
-import { Button, Row, Col, Input,
-         Card, CardBody, UncontrolledTooltip
-} from 'reactstrap';
+import { withRouter } from 'react-router-dom';
 import ReactTable from 'react-table';
-import Datetime from 'react-datetime';
 import moment from 'moment';
 import XLSX from 'xlsx';
 import firebase from './firebase.js';
+import Pagination from './TablePagination';
+import { Container, Button,
+  Row, Col, Card, CardHeader, CardBody,
+  Tooltip
+} from 'reactstrap';
 
-class Pupil {
-  name: String;
-  id: String;
-  phoneNumber: String;
-  birthDay: String;
-  whenRegistered: Timestamp;
-
-  constructor(recordId: String,
-              name: String,
-              id: String,
-              phoneNumber: String,
-              medicalLimitations: Boolean,
-              birthDay: String,
-              whenRegistered: Timestamp,
-              parentId: String,
-              address: String) {
-    this.recordId = recordId;
-    this.name = name;
-    this.id = id;
-    this.phoneNumber = phoneNumber;
-    this.medicalLimitations = medicalLimitations;
-    this.birthDay = birthDay;
-
-    this.whenRegistered = ( whenRegistered ) ?
-                          moment.unix(whenRegistered.seconds).format('DD/MM/YYYY HH:mm') :
-                          null;
-    this.parentId = parentId;
-    this.address = address;
-  }
-}
-
-class GroupData {
-  name: String;
-  symbol: String;
-  capacity: Number;
-
-  constructor(name: String,
-              symbol: String,
-              capacity: Number,
-              openFrom: Timestamp,
-              openTill: Timestamp) {
-    this.name = name;
-    this.symbol = symbol;
-    this.capacity = capacity;
-    this.openFrom = moment.unix(openFrom.seconds);
-    this.openTill = moment.unix(openTill.seconds);
-  }
+type Group = {
+    id: String,
+    unitId: String,
+    name: String,
+    symbol: String,
+    capacity: Number,
+    open: Date,
+    openTill: Date,
+    unitName: String,
+    price: Number
 }
 
 type State = {
-  pupils: Array<Pupil>,
-  groupData: GroupData,
-  dataStatus: String,
+  groups: Group[],
+  tooltipOpen: Boolean
 }
 
-class Group extends React.Component<{}, State> {
+class Groups extends React.Component<{}, State> {
 
   state = {
-    pupils: [],
-    groupData: {
-      name: '',
-      symbol: '',
-      openFrom: moment(),
-      openTill: moment()
-    },
-    dataStatus: 'טעינת נתונים..'
+    groups: [],
+    tooltipOpen: false
   }
 
   async componentDidMount() {
-    const groupId = this.props.match.params.groupid;
-    const unitId = this.props.match.params.unitid;
 
     const getOptions = {
       source: 'server'
     }
 
-    const groupDoc = firebase.firestore().collection('units')
-                    .doc(unitId).collection('groups')
-                    .doc(groupId);
+    const _groups = [];
+    const units = await firebase.firestore().collection('units')
+                       .get(getOptions);
 
-    try {
+    units.docs.forEach( async(unit) => {
 
-      const doc = await groupDoc.get(getOptions);
-      let data = doc.data();
-      const _groupData = new GroupData(data.name,
-                                       data.symbol,
-                                       data.capacity,
-                                       data.opened,
-                                       data.openedTill);
-      this.setState({
-        groupData: _groupData
-      })
+      const unitData = unit.data();
+      const unitId = unit.id;
+      const unitName = unitData.name_he;
 
-      this.observer = firebase.firestore().collection('units')
-                      .doc(unitId).collection('groups')
-                      .doc(groupId).collection('pupils')
-                      .onSnapshot( snapShot => {
-                        ::this.pupilsFromDocs(snapShot.docs);
-                      });
+      const groups = await firebase.firestore().collection('units')
+                    .doc(unit.id).collection('groups')
+                    .get(getOptions);
+      groups.docs.forEach( group => {
 
-    } catch( err ) {
-      console.error(err);
-    }
+          const groupData = group.data();
+          const groupId = group.id;
 
-  }
+          const openTill = groupData.openedTill ?
+                           moment.unix(groupData.openedTill.seconds).format('DD/MM/YYYY') :
+                           '';
 
-  componentWillUnmount() {
-    if( this.observer )
-      this.observer();
-  }
+          _groups.push({
+            id: groupId,
+            unitId: unitId,
+            name: groupData.name,
+            symbol: groupData.symbol,
+            unitName: unitName,
+            open: moment.unix(groupData.opened.seconds).format('DD/MM/YYYY'),
+            openTill: openTill,
+            price: groupData.price,
+            capacity: groupData.capacity
+          });
 
-  pupilsFromDocs(docs) {
+      });
 
-    const _pupils = [];
-    docs.forEach( (pupilDoc) => {
+    });
 
-      const pupilData = pupilDoc.data();
-      const _pupil = new Pupil(pupilDoc.id,
-                               pupilData.name,
-                               pupilData.pupilId,
-                               pupilData.phoneNumber,
-                               pupilData.medicalLimitations,
-                               pupilData.birthDay,
-                               pupilData.whenRegistered,
-                               pupilData.parentId,
-                               pupilData.address);
-
-      _pupils.push(_pupil);
+    this.setState({
+      groups: _groups
     })
 
-    if( _pupils.length == 0 ) {
-      this.setState({
-          dataStatus: 'עדיין לא נרשם אף אחד'
-      });
-    } else {
-      this.setState({
-        pupils: _pupils
-      });
-    }
+  }
+
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
   }
 
   exportExcel() {
@@ -155,30 +96,31 @@ class Group extends React.Component<{}, State> {
     const _export = {
       /* Array of Arrays e.g. [["a","b"],[1,2]] */
      data: [
-       ["", "שם", "ת.ז.", "מספר טלפון", "תאריך לידה", "תאריך הרשמה", "מזהה הורה", "כתובת"],
+       ["", "מחיר", "ת.סיום", "ת. התחלה", "שם מוסד", "כמות ילדים", "מזהה", "שם"],
      ],
     };
 
-    this.state.pupils.forEach( (pupil, index) => {
-      const pupilData = [];
-      pupilData.push(1 + index); // reserve 1 for caption row
-      pupilData.push(pupil.name);
-      pupilData.push(pupil.id);
-      pupilData.push(pupil.phoneNumber);
-      pupilData.push(pupil.birthDay);
-      pupilData.push(pupil.whenRegistered);
-      pupilData.push(pupil.parentId);
-      pupilData.push(pupil.address);
+    this.state.groups.forEach( (group, index) => {
+        const groupData = [];
+        groupData.push(1 + index); // reserve 1 for caption row
+        groupData.push(group.name);
+        pupilData.push(group.id);
+        groupData.push(group.symbol);
+        groupData.push(group.capacity);
+        groupData.push(group.unitName);
+        groupData.push(group.open);
+        groupData.push(group.openTill);
+        groupData.push(group.price);
 
-      _export.data.push(pupilData);
-    })
+        _export.data.push(groupData);
+    });
 
     /* create a new blank workbook */
     var workbook = XLSX.utils.book_new();
     console.log(workbook.Views);
     /* convert from array of arrays to workbook */
     var worksheet = XLSX.utils.aoa_to_sheet(_export.data);
-    XLSX.utils.book_append_sheet(workbook, worksheet, this.state.groupData.name);
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'כיתות');
 
     /* create view to RTL */
     if(!workbook.Workbook) workbook.Workbook = {};
@@ -187,54 +129,13 @@ class Group extends React.Component<{}, State> {
     workbook.Workbook.Views[0].RTL = true;
 
     /* write a workbook */
-    XLSX.writeFile(workbook, `${this.state.groupData.name}.xlsx`);
+    XLSX.writeFile(workbook, 'groups.xlsx');
+
   }
 
-  async updateFirestore(pupilIndex: Number,
+  async updateFirestore(groupIndex: Number,
                   fieldName: String,
                   value) {
-    if( value != null ) {
-
-      const groupId = this.props.match.params.groupid;
-      const unitId = this.props.match.params.unitid;
-
-      const data = [...this.state.pupils];
-      data[pupilIndex][fieldName] = value;
-      this.setState({
-        pupils: data
-      });
-
-      const pupilRecordId = data[pupilIndex].recordId;
-
-      try {
-        let json = {};
-        const updateField = fieldName;
-        json[updateField] = value;
-
-        await firebase.firestore().collection('units')
-                        .doc(unitId).collection('groups')
-                        .doc(groupId).collection('pupils')
-                        .doc(pupilRecordId)
-                        .update(json);
-
-      } catch( err ) {
-        console.error(err);
-      }
-
-    }
-  }
-
-  toggleIsMedicalLimitations(index: Number) {
-
-    const pupilData = this.state.pupils[index];
-    pupilData.medicalLimitations = !pupilData.medicalLimitations;
-    this.setState({
-      pupils: this.state.pupils
-    });
-
-    this.updateFirestore(index,
-                        'medicalLimitations',
-                        pupilData.medicalLimitations);
   }
 
   async handleUpdate(cellInfo, e) {
@@ -259,141 +160,93 @@ class Group extends React.Component<{}, State> {
       </div>)
   }
 
-  renderDatePicker(cellInfo, value) {
-    return(
-      <div style={{ backgroundColor: "#fafafa" }}
-            contentEditable
-            suppressContentEditableWarning
-            onKeyDown={ e => ::this.handleUpdate(cellInfo, e) }
-            onBlur={ e => ::this.handleUpdate(cellInfo, e) }>
-          {value}
-      </div>);
-  }
-
-  renderCheckable(cellInfo) {
-
-    const pupilData = this.state.pupils[cellInfo.index];
-    const _isLimitations = pupilData.medicalLimitations;
-
-    return (
-      <div className='form-check'
-           style={{
-             marginTop: '-16px'
-           }}>
-          <label className='form-check-label'>
-            <input className='form-check-input'
-                  type='checkbox'
-                  checked={_isLimitations}
-                  onChange={ () => ::this.toggleIsMedicalLimitations(cellInfo.index) }
-            />
-            <span className='form-check-sign'
-                  style={{
-                    paddingLeft: '10px;'
-                  }}>
-            </span>
-          </label>
-      </div>)
-
-  }
-
-  birthDayChanged() {
-
-  }
-
   render() {
-    return (
-      <div>
-        <UncontrolledTooltip placement='auto'
-          autohide={false}
-          style={{
-            backgroundColor: 'black',
-            color: 'white'
-          }}
-          target='btnExportExcel'>
-            ייצוא לקובץ חיצוני
-        </UncontrolledTooltip>
-        <div className='panel-header panel-header-sm'></div>
-        <div className='content container h-100'>
-          <Row>
-            <Col className='col-md-12'>
-              <Card>
-                <div className='card-header'>
-                  <h5 className='title' dir='rtl'>רישום תלמידים לכיתה {this.state.groupData.name} (מזהה {this.state.groupData.symbol}) </h5>
-                  <h5 className='title'>קיבולת: {this.state.groupData.capacity} ילדים</h5>
-                  <h5 className='title'>תאריכי פעילות: מ {this.state.groupData.openFrom.format('DD/MM/YYYY')} עד {this.state.groupData.openTill.format('DD/MM/YYYY')}</h5>
-                </div>
-                <CardBody>
-                  <Row>
-                    <Col md='12' className="text-right my-auto">
-                      <Button color='primary' id='btnExportExcel'
-                              onClick={::this.exportExcel}>Excel</Button>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md='12'>
-                      <ReactTable
-                        className="-striped -highlight tableInCard"
-                        data={this.state.pupils}
-                        noDataText={this.state.dataStatus}
-                        filterable
-                        getTheadThProps = { () => {
-                          return {
-                            style: {
-                              'textAlign': 'right'
+
+    const columns = [{
+      Header: 'שם',
+      accessor: 'name'
+    }, {
+      Header: 'מזהה',
+      accessor: 'symbol'
+    }, {
+      Header: 'כמות ילדים',
+      accessor: 'capacity'
+    }, {
+      Header: 'שם מוסד',
+      accessor: 'unitName'
+    }, {
+      Header: 'ת. התחלה',
+      accessor: 'open'
+    }, {
+      Header: 'ת.סיום',
+      accessor: 'openTill'
+    }, {
+      Header: 'מחיר',
+      accessor: 'price',
+      Cell: cellInfo => ::this.renderEditable(cellInfo, cellInfo.original.price)
+    }];
+
+    const self = this;
+
+    return <div>
+              <div className='panel-header panel-header-sm'></div>
+              <Container className='content h-100'>
+                <Row>
+                  <Col md='12'>
+                    <Card>
+                      <CardHeader>
+                        <h5 className='title'>ניהול כיתות</h5>
+                      </CardHeader>
+                      <CardBody>
+                        <Row>
+                          <Col md='12' className='text-right my-auto' id='tooltipContainer'>
+                              <Button color='primary' id='btnExportExcel'
+                                      onClick={::this.exportExcel}>Excel</Button>
+                              <Tooltip placement='auto'
+                                autohide={false}
+                                isOpen={this.state.tooltipOpen}
+                                toggle={::this.toggle}
+                                container='tooltipContainer'
+                                style={{
+                                  backgroundColor: 'black',
+                                  color: 'white'
+                                }}
+                                target='btnExportExcel'>
+                                  ייצוא לקובץ חיצוני
+                              </Tooltip>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col md='12'>
+                            <ReactTable
+                          filterable
+                          PaginationComponent={Pagination}
+                          getTheadThProps = { () => {
+                            return {
+                              style: {
+                                'textAlign': 'right'
+                              }
                             }
-                          }
-                        }}
-                        getTdProps = { (state, rowInfo, column, instance) => {
-                          return {
-                            style: {
-                              'textAlign': 'right'
-                            }
-                          }
-                        }}
-                        loadingText='טוען נתונים...'
-                        noDataText='טוען נתונים...'
-                        previousText = 'קודם'
-                        nextText = 'הבא'
-                        pageText = 'עמוד'
-                        ofText = 'מתוך'
-                        rowsText = 'שורות'
-                        columns={[{
-                          Header: 'שם',
-                          accessor: 'name',
-                          Cell: cellInfo => ::this.renderEditable(cellInfo, cellInfo.original.name)
-                        }, {
-                          Header: 'מזהה',
-                          accessor: 'id'
-                        }, {
-                          Header: 'מספר טלפון',
-                          accessor: 'phoneNumber'
-                        }, {
-                          Header: 'תאריך לידה',
-                          accessor: 'birthDay',
-                          Cell: cellInfo => ::this.renderDatePicker(cellInfo, cellInfo.original.birthDay)
-                        },{
-                          Header: 'תאריך הרשמה',
-                          accessor: 'whenRegistered'
-                        }, {
-                          Header: 'מזהה הורה',
-                          accessor: 'parentId',
-                          Cell: cellInfo => ::this.renderEditable(cellInfo, cellInfo.original.parentId)
-                        }, {
-                          Header: 'כתובת',
-                          accessor: 'address',
-                          Cell: cellInfo => ::this.renderEditable(cellInfo, cellInfo.original.address)
-                        }]}>
-                      </ReactTable>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    )
+                          }}
+                          loadingText='טוען נתונים...'
+                          noDataText='טוען נתונים...'
+                          previousText = 'קודם'
+                          nextText = 'הבא'
+                          pageText = 'עמוד'
+                          ofText = 'מתוך'
+                          rowsText = 'שורות'
+                          data={this.state.groups}
+                          columns={columns}/>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                </Row>
+              </Container>
+         </div>
   }
+
 }
 
-export default Group;
+export default withRouter(Groups)
