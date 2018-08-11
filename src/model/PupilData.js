c4a6e3ce85bc73035afda8787ea9cf2680207a96
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-11 20:42:26 +0300

    Update PupilData.js

--- src/model/PupilData.js
+++ src/model/PupilData.js
@@ -15,6 +15,7 @@ class Pupil {
 
   constructor(recordId: String,
               name: String,
+              lastName: String,
               id: String,
               phoneNumber: String,
               medicalLimitations: Boolean,
@@ -24,6 +25,7 @@ class Pupil {
               address: String) {
     this.recordId = ( recordId ) ? recordId : '';
     this.name = ( name ) ? name : '';
+    this.lastName = ( lastName ) ? lastName : '';
     this.id = ( id ) ? id: '';
     this.phoneNumber = ( phoneNumber ) ? phoneNumber : '';
     this.medicalLimitations = ( medicalLimitations ) ? medicalLimitations : false;
