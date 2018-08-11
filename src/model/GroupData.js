ec84139f076270d29874e60b18cbc497d72cf05d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-11 16:19:35 +0300

    Update GroupData.js

--- src/model/GroupData.js
+++ src/model/GroupData.js
@@ -1,26 +1,24 @@
-// @flow
-// type GroupData = {
-//   name: String,
-//   symbol: Number,
-//   openFrom: Date,
-//   openTill: Date,
-//   price: Number
-// }
+import moment from 'moment';
 
 export default
 class GroupData {
   name: String;
   symbol: String;
   capacity: Number;
+  price: Number;
+  openFrom: Number;
+  openTill: Number;
 
   constructor(name: String,
               symbol: String,
               capacity: Number,
+              price: Number,
               openFrom: Timestamp,
               openTill: Timestamp) {
     this.name = name;
     this.symbol = symbol;
     this.capacity = capacity;
+    this.price = price;
     if( openFrom )
       this.openFrom = moment.unix(openFrom.seconds);
     if( openTill )
