0799c634a8c04f6958476a0db27d1f441bd3e26a
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-11 20:30:48 +0300

    Add files via upload

---
+++ src/model/PupilData.js
@@ -0,0 +1,40 @@
+// @flow
+import moment from 'moment';
+
+export default
+class Pupil {
+  recordId: String;
+  name: String;
+  id: String;
+  phoneNumber: String;
+  medicalLimitations: Boolean;
+  birthDay: String;
+  whenRegistered: Timestamp;
+  parentId: String;
+  address: String;
+
+  constructor(recordId: String,
+              name: String,
+              id: String,
+              phoneNumber: String,
+              medicalLimitations: Boolean,
+              birthDay: String,
+              whenRegistered: Timestamp,
+              parentId: String,
+              address: String) {
+    this.recordId = ( recordId ) ? recordId : '';
+    this.name = ( name ) ? name : '';
+    this.id = ( id ) ? id: '';
+    this.phoneNumber = ( phoneNumber ) ? phoneNumber : '';
+    this.medicalLimitations = ( medicalLimitations ) ? medicalLimitations : false;
+    this.birthDay = ( birthDay ) ?
+                          moment.unix(birthDay.seconds).format('DD/MM/YYYY') :
+                          null;
+
+    this.whenRegistered = ( whenRegistered ) ?
+                          moment.unix(whenRegistered.seconds).format('DD/MM/YYYY HH:mm') :
+                          null;
+    this.parentId = ( parentId ) ? parentId : '';
+    this.address = ( address ) ? address : '';
+  }
+}
