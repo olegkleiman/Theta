9e4c5dd189bfc9b23989c338e6395108e6da9c04
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-12 04:30:33 +0300

    Update PupilData.js

--- src/model/PupilData.js
+++ src/model/PupilData.js
@@ -5,6 +5,7 @@ export default
 class Pupil {
   recordId: String;
   name: String;
+  lastName: String;
   id: String;
   phoneNumber: String;
   medicalLimitations: Boolean;
@@ -24,12 +25,12 @@ class Pupil {
               parentId: String,
               address: String) {
 
-    this.recordId = ( recordId ) ? recordId : '';
-    this.name = ( name ) ? name : '';
-    this.lastName = ( lastName ) ? lastName : '';
-    this.id = ( id ) ? id: '';
-    this.phoneNumber = ( phoneNumber ) ? phoneNumber : '';
-    this.medicalLimitations = ( medicalLimitations ) ? medicalLimitations : false;
+    this.recordId = ( recordId ||  '');
+    this.name = ( name ||  '');
+    this.lastName = ( lastName || '');
+    this.id = ( id || '');
+    this.phoneNumber = ( phoneNumber || '');
+    this.medicalLimitations = medicalLimitations;
     this.birthDay = ( birthDay ) ?
                           moment.unix(birthDay.seconds).format('DD/MM/YYYY') :
                           null;
@@ -39,7 +40,7 @@ class Pupil {
                           .utcOffset(0)
                           .format('DD/MM/YYYY HH:mm') :
                           null;
-    this.parentId = ( parentId ) ? parentId : '';
-    this.address = ( address ) ? address : '';
+    this.parentId = ( parentId || '');
+    this.address = ( address ||  '');
   }
 }
