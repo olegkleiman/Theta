45c91e0f2cce6ef7e26372d65c76e6f9f4de9544
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-13 02:37:30 +0300

    Update Group.jsx

--- src/Group.jsx
+++ src/Group.jsx
@@ -243,17 +243,15 @@ class Group extends React.Component<{}, State> {
   }
 
   async deletePupil() {
-
     this.setState({
       modal: !this.state.modal,
       pupilId2Delete: ''
     });
 
-    const deleteDoc = await firebase.firestore()
-                    .collection('units').doc(this.props.match.params.unitid)
-                    .collection('groups').doc(this.props.match.params.groupid)
-                    .collection('pupils').doc(this.state.pupilId2Delete)
-                    .delete();
+    const deleteDoc = await firebase.firestore().collection('units').doc(this.props.match.params.unitid)
+        .collection('groups').doc(this.props.match.params.groupid)
+        .collection('pupils').doc(this.state.pupilId2Delete)
+        .delete();
     console.log(deleteDoc);
   }
 
