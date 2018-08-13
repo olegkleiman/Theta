95a158471a3592cabf85a1021f4112997132d0f7
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-08-13 17:46:14 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/database';
 import 'firebase/firestore';
-import 'firebase/storage';
 
 var config = {
   apiKey: "AIzaSyDi0kLfYSZ9ofQCNDdT9QZWwJfgdth8SR4",
